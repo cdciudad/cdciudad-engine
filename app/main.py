9b35a05ead3010e10b542a5a7e0c5be29443d9f7
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-03-05 01:31:48 -0600

    [add] mail service and logger

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 # Fast API
 from fastapi import FastAPI
+from app.email.service import send_email
 from app.routers import subscribers
 
 app = FastAPI()
+send_email()
 
 # Routes
 app.include_router(subscribers.router)
