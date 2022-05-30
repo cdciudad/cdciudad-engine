12cf10628f2dd214fda53ef75330e9ef3fe0b665
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-05-30 02:16:56 -0600

    [fix] history content divided by paragraph in a list

--- app/src/models/history.py
+++ app/src/models/history.py
@@ -19,7 +19,7 @@ class HistoryPost(BaseModel):
     id: UUID = Field(default=uuid4(), alias='_id')
 
     title: str = Field(..., example="The Office")
-    content: str = Field(..., example="The Office is an American mockumentary sitcom television series that depicts the everyday work lives of office employees in the Scranton, Pennsylvania branch of the fictional Dunder Mifflin Paper Company. It aired on NBC from March 24, 2005, to May 16, 2013, spanning a total of nine seasons.")
+    content: List[str] = Field(..., example=["The Office is an American mockumentary sitcom television series that depicts the everyday work lives of office employees in the Scranton, Pennsylvania branch of the fictional Dunder Mifflin Paper Company. It aired on NBC from March 24, 2005, to May 16, 2013, spanning a total of nine seasons."])
 
     gallery: List[str] = Field(..., example=["https://www.alohacriticon.com/wp-content/uploads/2020/02/the-office-tv-serie-sinopsis.jpg",
                                "https://www.fiebreseries.com/wp-content/uploads/2021/04/The-Office_poster_serie0-1-332x381.jpg", "https://www.abc.es/media/series/000/001/848/the-office-2.jpg"])
